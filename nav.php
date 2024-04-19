28e41147fa840882c4d74b6e8432e93142e34e7f
sssssshiba <trunghieu061220@gmail.com>
2024-04-19 13:08:15 +0900

    navigation bar fixed

--- nav.php
+++ nav.php
@@ -1,13 +1,10 @@
 <link rel="stylesheet" href="styles/nav.css">
 
 <nav>
-    <ul>
-        <li><a href="home.php">ホーム</a></li>
-        <li><a href="profile">プロフィール</a></li>  <!-- リンク先なし -->
-        <li><a href="view.php">作品閲覧</a></li>
-        <li><a href="view.php">記事閲覧</a></li>    <!-- リンク先未作成 -->
-        <li><a href="upload.php">アップロード</a></li>
-        <li><a href="logout">ログアウト</a></li> <!-- リンク先なし -->
-
-    </ul>
+    <a href="logout">ログアウト</a> <!-- リンク先なし -->
+    <a href="upload.php">アップロード</a>
+    <a href="view.php">記事閲覧</a>    <!-- リンク先未作成 -->
+    <a href="view.php">作品閲覧</a>
+    <a href="profile">プロフィール</a>  <!-- リンク先なし -->
+    <a href="home.php">ホーム</a>
 </nav>
