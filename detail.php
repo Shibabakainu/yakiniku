70e38630d541712e2489fcee1bf6d84c0200fad3
st0H034 <kd1307152@st.kobedenshi.ac.jp>
2024-04-24 14:53:49 +0900

    add 萩原: グッドボタンの追加に合わせてレイアウトの調整、背景色の変更

--- detail.php
+++ detail.php
@@ -13,21 +13,26 @@ require_once __DIR__ . '/nav.php';
     <form class="view_form">
         <h2 class="detail_h2">ChatGPTに聞いてみた！</h2>
         <p class="name">投稿者：神戸　太郎</p>
-        <p>
+        <p class="report_body">
             皆さんはChatGPTをご存知ですか？<br>
             アドバイスやアイデアを出してくれたりとさまざまなことに役立つAIチャットサービスです。<br>
             これはそんなChatGPTにいろんなことを聞いて、知ってもらおうという企画です！<br>
             記念すべき第1回目は、今話題の「スクラム開発」について聞いてみました！<br>
         </p>
         <img src="images/chatgpt.jpg" class="image" alt="ここに画像が表示されます"><br>
-        <p>
+        <p class="report_body">
             このように知りたいことをまとめて教えてくれました。<br>
             次回は、スクラム開発の中で行われる、「デイリースクラム」についてです。
         </p>
 
         <?php
             require_once __DIR__ . '/DownloadButton.php';   // ダウンロードボタンの呼び出し
-        ?>
+        ?><br>
+
+    <?php
+        require_once __DIR__ . '/GoodButton.php';   // グッドボタンの呼び出し
+    ?>
+
 
         <input type="button" class="return_button" onclick="location.href='./view.php'" value="戻る">
 
