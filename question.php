da0c4128446865854c56da6905a5fc50499dee64
sssssshiba <trunghieu061220@gmail.com>
2024-04-25 11:12:00 +0900

    ヒエウ：css調整

--- question.php
+++ question.php
@@ -12,16 +12,18 @@ require_once __DIR__ . '/nav.php';
 </head>
 
 <body>
-    <h3>質問投稿</h3>
-    <form action="./finish.php" method="POST" enctype="multipart/form-data">
-        <p>質問内容<br>
-            <textarea name="content"></textarea>
-        </p>
-        <p>画像<br>
-            <input type="file" name="image" accept=".jpg, .png">
-        </p>
-        <button type="submit" class="submit" formaction="http://localhost/yakiniku/finish.php">投稿</button>
-    </form>
+    <div class="wrap">
+        <h3>質問投稿</h3>
+        <form action="./finish.php" method="POST" enctype="multipart/form-data">
+            <p>質問内容<br>
+                <textarea name="content"></textarea>
+            </p>
+            <p>画像<br>
+                <input type="file" name="image" accept=".jpg, .png">
+            </p>
+            <button type="submit" class="submit" formaction="http://localhost/yakiniku/finish.php">投稿</button>
+        </form>
+    </div>
 </body>
 
 </html>
--- view.php
+++ view.php
@@ -31,34 +31,36 @@ require_once __DIR__ . '/nav.php';
     </head>
 
     <body>
-        <p class="view_p">投稿作品一覧</p>
-        <table border="1" style="border-collapse: collapse">
-            <tr bgcolor="#e6e6fa">
-                <td width="400px">タイトル</td>
-                <td width="800px">作品の説明</td>
-            </tr>
-            <tr>
-                <td><a href="./detail.php"><span style="color: black;">ChatGPTに聞いてみた！</span></a></td>
-                <th>
-                    ChatGPTに今話題のデイリースクラムについて聞いてみた！
-                    <?php
-                        require_once __DIR__ . '/GoodButton.php';   // グッドボタンの呼び出し
-                    ?>
-                </th>
-            </tr>
-            <tr>
-                <td><a href="./detail.php"><span style="color: black;">「ゼロから始める機械学習」: 機械学習入門ガイド</span></a></td>
-                <th>この記事では、機械学習の基本から応用までを解説します。アルゴリズム、データセット、モデルの選択方法などについて詳しく説明します。</th>
-            </tr>
-            <tr>
-                <td><a href="./detail.php"><span style="color: black;">「最新のフロントエンド開発トレンド」: 2023年版</span></a></td>
-                <th>フロントエンド開発の最新トレンドを紹介する記事です。Webコンポーネント、TypeScript、モバイルフレームワークなどに焦点を当てています。</th>
-            </tr>
-            <tr>
-                <td><a href="./detail.php"><span style="color: black;">「サーバーレスアーキテクチャの実践的ガイド」: AWS Lambda、Azure Functions、Google Cloud Functionsの比較</span></a></td>
-                <th>サーバーレスアーキテクチャの選択肢と、それぞれのプラットフォームでの実装方法を比較する記事です。関数のデプロイ、スケーリング、コストなどについて詳しく解説します。</th>
-            </tr>
-        </table>
+        <div class="wrap">
+            <p class="view_p">投稿作品一覧</p>
+            <table border="1" style="border-collapse: collapse">
+                <tr bgcolor="#e6e6fa">
+                    <td width="400px">タイトル</td>
+                    <td width="800px">作品の説明</td>
+                </tr>
+                <tr>
+                    <td><a href="./detail.php"><span style="color: black;">ChatGPTに聞いてみた！</span></a></td>
+                    <th>
+                        ChatGPTに今話題のデイリースクラムについて聞いてみた！
+                        <?php
+                            require_once __DIR__ . '/GoodButton.php';   // グッドボタンの呼び出し
+                        ?>
+                    </th>
+                </tr>
+                <tr>
+                    <td><a href="./detail.php"><span style="color: black;">「ゼロから始める機械学習」: 機械学習入門ガイド</span></a></td>
+                    <th>この記事では、機械学習の基本から応用までを解説します。アルゴリズム、データセット、モデルの選択方法などについて詳しく説明します。</th>
+                </tr>
+                <tr>
+                    <td><a href="./detail.php"><span style="color: black;">「最新のフロントエンド開発トレンド」: 2023年版</span></a></td>
+                    <th>フロントエンド開発の最新トレンドを紹介する記事です。Webコンポーネント、TypeScript、モバイルフレームワークなどに焦点を当てています。</th>
+                </tr>
+                <tr>
+                    <td><a href="./detail.php"><span style="color: black;">「サーバーレスアーキテクチャの実践的ガイド」: AWS Lambda、Azure Functions、Google Cloud Functionsの比較</span></a></td>
+                    <th>サーバーレスアーキテクチャの選択肢と、それぞれのプラットフォームでの実装方法を比較する記事です。関数のデプロイ、スケーリング、コストなどについて詳しく解説します。</th>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
 
