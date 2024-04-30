6fd0f0bc6326f89fd0448fbd879417e9cce4654d
Atuy0000 <kd1302867@st.kobedenshi.ac.jp>
2024-04-30 10:47:45 +0900

    4/30 阪邊

--- nav.php
+++ nav.php
@@ -5,7 +5,7 @@
     <a href="login.php"><span class="material-symbols-outlined">logout</span>ログアウト</a>
     <a href="upload.php"><span class="material-symbols-outlined">drive_folder_upload</span>作品を投稿</a>
     <a href="question.php"><span class="material-symbols-outlined">quiz</span>質問を投稿</a>
-    <a href="view.php"><span class="material-symbols-outlined">quick_reference_all</span>質問閲覧</a> <!-- リンク先未作成 -->
+    <a href="question_list.php"><span class="material-symbols-outlined">quick_reference_all</span>質問閲覧</a> <!-- リンク先未作成 -->
     <a href="view.php"><span class="material-symbols-outlined">preview</span>作品閲覧</a>
     <a href="demoprofile.php"><span class="material-symbols-outlined">account_circle</span>プロフィール</a>
     <a href="home.php"><span class="material-symbols-outlined">home</span>ホーム</a>
